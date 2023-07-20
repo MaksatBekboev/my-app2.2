11b7d0368d7fefae8e643b12fa28ae51d2b90a7a
MaksatBekboev <132592881+MaksatBekboev@users.noreply.github.com>
2023-07-20 21:29:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import InputFields from './InputFields';
+import ResultDisplay from './ResultDisplay';
+import Notification from './Notification';
+import { calculate } from './actions';
 
-function App() {
+const App = () => {
+  const result = useSelector((state) => state.result);
+  const error = useSelector((state) => state.error);
+  const dispatch = useDispatch();
+
+  const handleCalculate = (operator, num1, num2) => {
+    if (isNaN(num1) || isNaN(num2)) {
+      dispatch({ type: 'ERROR', payload: { error: 'Некорректные числа!' } });
+      return;
+    }
+
+    const result = calculate(operator, num1, num2);
+    dispatch({ type: 'CALCULATE', payload: { result } });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <InputFields onCalculate={handleCalculate} />
+      {result !== null && <ResultDisplay result={result} />}
+      {error && <Notification message={error} />}
     </div>
   );
-}
+};
 
 export default App;
